5da53624f08d27aa67901f004902e8ec8c078b7d
Kyng Cytro <johndibashi2001@gamil.com>
2024-04-17 14:30:59 +0100

    fixed type issues

--- composables/useFetchApi.ts
+++ composables/useFetchApi.ts
@@ -1,10 +1,10 @@
-export default (url: any, options = {}) => {
+export default (url: any, options?: any) => {
   const { useAuthToken } = useAuth();
 
   return $fetch(url, {
     ...options,
     headers: {
-      ...options.headers,
+      ...options?.headers,
       Authorization: `Bearer ${useAuthToken().value}`,
     },
   });
