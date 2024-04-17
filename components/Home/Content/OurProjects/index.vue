20b3c40861b9564020cffcf877dfa6945af32ad9
Michael Akpuofoba <67930856+michaelkyng@users.noreply.github.com>
2024-04-17 20:58:17 +0100

    Update index.vue

--- components/Home/Content/OurProjects/index.vue
+++ components/Home/Content/OurProjects/index.vue
@@ -17,7 +17,7 @@
         :target="cardItem.target"
         :completed="cardItem.completed"
         :image="cardItem.mediaFiles[0].url"
-        :path="`/projects/f/${cardItem.title}`"
+        :path="`${cardItem.title}`"
         >
             
         </HomeContentOurProjectsCard>
