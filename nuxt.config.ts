8e2be7fad440086cf13cc75c146df23ce506ca38
Michael Akpuofoba <67930856+michaelkyng@users.noreply.github.com>
2025-09-11 14:25:33 +0100

    refact : Removed Vercel Json

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -81,7 +81,7 @@ export default defineNuxtConfig({
     public: {
       email: "",
       waitList: process.env.NUXT_PUBLIC_WAIT_LIST,
-      apiBaseUrl: process.env.NUXT_PUBLIC_API_BASE_URL || '',
+      apiBaseUrl: process.env.NUXT_PUBLIC_API_BASE_URL || 'localhost:3080',
       cloudinaryCloudName: process.env.NUXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
       cloudinaryUploadPreset: process.env.NUXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET
     },
@@ -90,11 +90,6 @@ export default defineNuxtConfig({
     smtpUser: process.env.SMTP_USER,
     smtpPass: process.env.SMTP_PASS,
     adminEmail: process.env.ADMIN_EMAIL
-  },
-  routeRules: {
-    '/api/admin/**': { proxy: { to: 'https://api.fortrez.io/admin/**' } },
-    '/api/auth/**': { proxy: { to: 'https://api.fortrez.io/auth/**' } },
-    '/api/**': { proxy: { to: 'https://api.fortrez.io/**' } }
   },
   typescript: {
     strict: true,
